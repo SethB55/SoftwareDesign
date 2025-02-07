bbb0d47d720b77101c3f31ab46fff42e39391d24
sebolen <sebolen@uiowa.edu>
2025-02-07 16:52:11 -0600

    big yikes

--- homework0/HW0_ElementarySchool/src/Classroom.java
+++ homework0/HW0_ElementarySchool/src/Classroom.java
@@ -3,17 +3,37 @@ public class Classroom {
     private int gradeLevel;
     private Teacher teacher;
     private ArrayList<Student> students;
-    public Classroom(){
+
+    public Classroom(Teacher t1, int gradeLevel) {
+        this.teacher = teacher;
+        this.gradeLevel = gradeLevel;
+    }
 
 
-    Classroom(Teacher teacher, int gradeLevel) {
+    public void Classroom(Teacher teacher, int gradeLevel, ArrayList students) {
             this.teacher = teacher;
             this.gradeLevel = gradeLevel;
-            this.students = new ArrayList<>();
+            this.students = students;
+        }
+
+    public void rollCall(){
+        System.out.print("Roll Call: ");
+        for(int i =0; i < students.size(); i++){
+            System.out.print(students.get(i));
+            System.out.print(students.get(i).getLunchPreference());
         }
     }
-    public int rollCall(){
-        return
+
+    public String generateLunchReport() {
+        int hotCount = 0, coldCount = 0;
+        for (Student student : students) {
+            if (student.getLunchPreference().equalsIgnoreCase("Hot")) {
+                hotCount++;
+            } else {
+                coldCount++;
+            }
+        }
+        return "Hot lunch: " + hotCount + ", Cold lunch: " + coldCount;
     }
     public int getGradeLevel() {
         return gradeLevel;
@@ -23,5 +43,14 @@ public class Classroom {
         this.gradeLevel = gradeLevel;
     }
 
+    public String getID() {
+        return teacher.getName().charAt(0) + String.valueOf(gradeLevel);
+    }
+
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
 
 }
+
