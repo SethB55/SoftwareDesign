f045980835a6427bfc842d08f1120ca33bae0d75
sebolen <sebolen@uiowa.edu>
2025-02-24 21:29:03 -0600

    First commit, build and rebuild is fucked because i accidently deleted javaFX i believe

--- textbook_examples/ch18/fig18_20/src/Fractal.java
+++ textbook_examples/ch18/fig18_20/src/Fractal.java
@@ -1,5 +1,5 @@
 // Fractal.java
-import javafx.application.Application;
+import javafx.applicationSETH .Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
