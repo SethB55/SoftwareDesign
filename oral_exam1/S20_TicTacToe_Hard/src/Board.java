0157289b36ca933c17a177ef035c7183527025ac
sebolen <sebolen@uiowa.edu>
2025-02-26 00:36:41 -0600

    idek bruh, future seth next commit will be big trust

---
+++ oral_exam1/S20_TicTacToe_Hard/src/Board.java
@@ -0,0 +1,8 @@
+// BOARD CLASS OUTLINE BEFORE CODING
+//Holds the current board configuration and ensures only valid moves are made.
+//AddX and AddO methods that add to the board but must check if valid
+//Check Win method: Must check after every valid move made if there is a winner or both losers
+// reset method is called and resets the board
+public class Board {
+
+}
---
+++ oral_exam1/S20_TicTacToe_Hard/src/Player.java
@@ -0,0 +1,20 @@
+//PLAYER CLASS OUTLINE BEFORE CODING
+//Parent class for both the ComputerPlayer and HumanPlayer classes
+public abstract class Player {
+    protected String name;
+    protected char side;
+
+    public Player(String name, char side) {
+        this.name = name;
+        this.side = side;
+    }
+    //initialize board
+    public abstract void makeMove(Board board);
+    //getters
+    public String getName() {
+        return name;
+    }
+    public char getSide() {
+        return side;
+    }
+}
