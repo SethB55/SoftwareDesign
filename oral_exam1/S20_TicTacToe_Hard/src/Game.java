00c17ced1f243976c008003ea21bb271884de028
sebolen <sebolen@uiowa.edu>
2025-03-07 15:58:46 -0600

    finishing up a lot of work

--- oral_exam1/S20_TicTacToe_Hard/src/Game.java
+++ oral_exam1/S20_TicTacToe_Hard/src/Game.java
@@ -1,36 +1,52 @@
 //MAIN GAME LOOP THAT WE RUN TO USE EVERY PART OF OUR FUNCTIONALITY
 //NEED PRIVATE BOARD AND PLAYERS
+/**
+ * Represents the main game loop for a Tic-Tac-Toe game.
+ * Manages the board and players, enforcing game rules and turn-taking.
+ */
 public class Game {
     private Board board;
     private Player player1;
     private Player player2;
-    public Game(Player player1, Player player2){
+
+    /**
+     * Constructs a Game instance with two players.
+     *
+     * @param player1 The first player.
+     * @param player2 The second player.
+     */
+    public Game(Player player1, Player player2) {
         this.board = new Board();
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    public void startGame(){
+    /**
+     * Starts and runs the Tic-Tac-Toe game loop.
+     * Resets the board, takes turns making moves, checks for a winner or draw,
+     * and displays the board state after each move.
+     */
+    public void startGame() {
         board.resetBoard();
         Player currentPlayer = player1;
         board.displayBoard();
 
-        while(true){
+        while (true) {
             currentPlayer.makeMove(board);
             board.displayBoard();
 
-            if(board.checkWin(currentPlayer.getSide())){
-                System.out.println(currentPlayer.getSide() + "Wins tic tac toe!");
+            if (board.checkWin(currentPlayer.getSide())) {
+                System.out.println(currentPlayer.getSide() + " Wins tic-tac-toe!");
                 break;
             }
 
-            if(board.checkFUll()){
+            if (board.checkFUll()) {
                 System.out.println("CAT! (It's a draw!)");
                 break;
             }
 
-            //switch turns
-            if(currentPlayer == player1){
+            // Switch turns
+            if (currentPlayer == player1) {
                 currentPlayer = player2;
             } else {
                 currentPlayer = player1;
--- oral_exam1/S55_Matrix_Hard/src/Junit.java
+++ oral_exam1/S55_Matrix_Hard/src/Junit.java
@@ -1,241 +1,124 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Junit {
 
     @Test
-    void isEqual(){
-        double[][] test1 = {{1, 2, 3}, {4, 5, 6}};
-        double [][] test2 = {{2,3,4}, {5,6,7}};
-        Matrix m1 = new Matrix(test1);
-        Matrix m2 = new Matrix(test2);
+    void testEquality() {
+        // Tests if two matrices are equal
+        double[][] arrayA = {{2, 4, 6}, {8, 10, 12}};
+        double[][] arrayB = {{2, 4, 6}, {8, 10, 12}};
+        double[][] arrayC = {{1, 3, 5}, {7, 9, 11}};
 
-        assertEquals(m1.equals(m1),true);
-        assertEquals(m1.equals(m2),false);
+        Matrix matrix1 = new Matrix(arrayA);
+        Matrix matrix2 = new Matrix(arrayB);
+        Matrix matrix3 = new Matrix(arrayC);
+
+        assertTrue(matrix1.equals(matrix2)); // Matrices are identical
+        assertFalse(matrix1.equals(matrix3)); // Matrices are different
     }
 
     @Test
-    void identityTest(){
-        Matrix m1 = Matrix.identity(2);
-        Matrix m2 = Matrix.identity(5);
+    void testIdentityMatrix() {
+        // Tests if identity matrix is correctly generated
+        Matrix identity2 = Matrix.identity(2);
+        Matrix identity3 = Matrix.identity(3);
 
-        assertEquals(m1.toString(),"1.0 0.0 \n" +
-                "0.0 1.0 \n");
-        assertEquals(m2.toString(),
-                "1.0 0.0 0.0 0.0 0.0 \n"+
-                        "0.0 1.0 0.0 0.0 0.0 \n"+
-                        "0.0 0.0 1.0 0.0 0.0 \n"+
-                        "0.0 0.0 0.0 1.0 0.0 \n"+
-                        "0.0 0.0 0.0 0.0 1.0 \n");
+        assertEquals("1.0 0.0 \n0.0 1.0 \n", identity2.toString());
+        assertEquals("1.0 0.0 0.0 \n0.0 1.0 0.0 \n0.0 0.0 1.0 \n", identity3.toString());
     }
 
     @Test
-    void subMatrixTest(){
-        double[][] test1 = {{1, 2, 3}, {4, 5, 6}};
-        double [][] test2 = {{1,1,1}, {2,2,2},{3,3,3}};
-        Matrix m1 = new Matrix(test1);
-        Matrix m2 = new Matrix(test2);
+    void testSubMatrix() {
+        // Extracts a submatrix from a given matrix
+        double[][] array = {{3, 6, 9}, {12, 15, 18}, {21, 24, 27}};
+        Matrix matrix = new Matrix(array);
 
-        Matrix m3 = m1.subMatrix(0,2,0,1);
-        Matrix m4 = m2.subMatrix(1,3,0,2);
+        Matrix sub1 = matrix.subMatrix(0, 2, 1, 2);
+        Matrix sub2 = matrix.subMatrix(1, 3, 0, 2);
 
-        assertEquals(m3.toString(),"1.0 \n"+
-                "4.0 \n");
-        assertEquals(m4.toString(),"2.0 2.0 \n"+
-                "3.0 3.0 \n");
+        assertEquals("6.0 9.0 \n15.0 18.0 \n", sub1.toString());
+        assertEquals("12.0 15.0 \n21.0 24.0 \n", sub2.toString());
     }
 
-
-
     @Test
-    void addTests(){
-        double[][] test1 = {{1, 2, 3}, {4, 5, 6}};
-        double [][] test2 = {{2,3,4}, {5,6,7}};
-        double [][] test3 = {{-2,-3,-4}, {-5,-6,-7}};
-        double [][] test4 = {{1,1,1}, {2,2,2},{3,3,3}};
-        double [][] test5 = {{2,2,2}, {3,3,3},{4,4,4}};
-        Matrix m1 = new Matrix(test1);
-        Matrix m2 = new Matrix(test2);
-        Matrix m3 = new Matrix(test3);
-        Matrix m4 = new Matrix(test4);
-        Matrix m5 = new Matrix(test5);
+    void testAddition() {
+        // Tests matrix addition (static, instance, and in-place)
+        double[][] array1 = {{1, 2}, {3, 4}};
+        double[][] array2 = {{5, 6}, {7, 8}};
+        Matrix m1 = new Matrix(array1);
+        Matrix m2 = new Matrix(array2);
 
-        assertEquals(Matrix.add(m1,m2).toString(),"3.0 5.0 7.0 \n" +
-                "9.0 11.0 13.0 \n"); // Static Tests
-        assertEquals(Matrix.add(m3,m2).toString(),"0.0 0.0 0.0 \n" +
-                "0.0 0.0 0.0 \n");
-        assertEquals(Matrix.add(m4,m5).toString(), "3.0 3.0 3.0 \n"+
-                "5.0 5.0 5.0 \n"+
-                "7.0 7.0 7.0 \n");
-
-        assertEquals(m1.add(m2).toString(),"3.0 5.0 7.0 \n" +
-                "9.0 11.0 13.0 \n"); // Instance Tests
-        assertEquals(m3.add(m2).toString(),"0.0 0.0 0.0 \n" +
-                "0.0 0.0 0.0 \n");
-        assertEquals(m4.add(m5).toString(),"3.0 3.0 3.0 \n"+
-                "5.0 5.0 5.0 \n"+
-                "7.0 7.0 7.0 \n");
+        assertEquals("6.0 8.0 \n10.0 12.0 \n", Matrix.add(m1, m2).toString());
+        assertEquals("6.0 8.0 \n10.0 12.0 \n", m1.add(m2).toString());
 
         m1.addInPlace(m2);
-        assertEquals(m1.toString(),"3.0 5.0 7.0 \n" +
-                "9.0 11.0 13.0 \n");
-
-        m1.addInPlace(m3);
-        assertEquals(m1.toString(),"1.0 2.0 3.0 \n" +
-                "4.0 5.0 6.0 \n");
-
-        m4.addInPlace(m5);
-        assertEquals(m4.toString(),"3.0 3.0 3.0 \n"+
-                "5.0 5.0 5.0 \n"+
-                "7.0 7.0 7.0 \n");
-
+        assertEquals("6.0 8.0 \n10.0 12.0 \n", m1.toString());
     }
 
     @Test
-    void subtractTests(){
-        double[][] test1 = {{1, 2, 3}, {4, 5, 6}};
-        double [][] test2 = {{2,3,4}, {5,6,7}};
-        double [][] test3 = {{-2,-3,-4}, {-5,-6,-7}};
-        double [][] test4 = {{1,1,1}, {2,2,2},{3,3,3}};
-        double [][] test5 = {{2,2,2}, {3,3,3},{4,4,4}};
-        Matrix m1 = new Matrix(test1);
-        Matrix m2 = new Matrix(test2);
-        Matrix m3 = new Matrix(test3);
-        Matrix m4 = new Matrix(test4);
-        Matrix m5 = new Matrix(test5);
+    void testSubtraction() {
+        // Tests matrix subtraction (static, instance, and in-place)
+        double[][] array1 = {{10, 9}, {8, 7}};
+        double[][] array2 = {{4, 3}, {2, 1}};
+        Matrix m1 = new Matrix(array1);
+        Matrix m2 = new Matrix(array2);
 
-        assertEquals(Matrix.subtract(m1,m2).toString(),"-1.0 -1.0 -1.0 \n" +
-                "-1.0 -1.0 -1.0 \n"); // Static Tests
-        assertEquals(Matrix.subtract(m3,m2).toString(),"-4.0 -6.0 -8.0 \n" +
-                "-10.0 -12.0 -14.0 \n");
-        assertEquals(Matrix.subtract(m4,m5).toString(), "-1.0 -1.0 -1.0 \n"+
-                "-1.0 -1.0 -1.0 \n"+
-                "-1.0 -1.0 -1.0 \n");
-
-
-        assertEquals(m1.subtract(m2).toString(),"-1.0 -1.0 -1.0 \n" +
-                "-1.0 -1.0 -1.0 \n"); // Instance Tests
-        assertEquals(m3.subtract(m2).toString(),"-4.0 -6.0 -8.0 \n" +
-                "-10.0 -12.0 -14.0 \n");
-        assertEquals(m4.subtract(m5).toString(),"-1.0 -1.0 -1.0 \n"+
-                "-1.0 -1.0 -1.0 \n"+
-                "-1.0 -1.0 -1.0 \n");
+        assertEquals("6.0 6.0 \n6.0 6.0 \n", Matrix.subtract(m1, m2).toString());
+        assertEquals("6.0 6.0 \n6.0 6.0 \n", m1.subtract(m2).toString());
 
         m1.subtractInPlace(m2);
-        assertEquals(m1.toString(),"-1.0 -1.0 -1.0 \n" +
-                "-1.0 -1.0 -1.0 \n");
-
-        m1.subtractInPlace(m3);
-        assertEquals(m1.toString(),"1.0 2.0 3.0 \n" +
-                "4.0 5.0 6.0 \n");
-
-        m4.subtractInPlace(m5);
-        assertEquals(m4.toString(),"-1.0 -1.0 -1.0 \n"+
-                "-1.0 -1.0 -1.0 \n"+
-                "-1.0 -1.0 -1.0 \n");
-
-
+        assertEquals("6.0 6.0 \n6.0 6.0 \n", m1.toString());
     }
 
     @Test
-    void scalerTests(){
-        double[][] test1 = {{1, 2, 3}, {4, 5, 6}};
-        double [][] test2 = {{2,2,2}, {3,3,3},{4,4,4}};
-        Matrix m1 = new Matrix(test1);
-        Matrix m2 = new Matrix(test2);
+    void testScalarMultiplication() {
+        // Tests scalar multiplication (static, instance, and in-place)
+        double[][] array = {{2, 4}, {6, 8}};
+        Matrix m = new Matrix(array);
 
-        assertEquals(Matrix.scalarMultiply(m1,2).toString(),"2.0 4.0 6.0 \n" +
-                "8.0 10.0 12.0 \n"); // Static Tests
-        assertEquals(Matrix.scalarMultiply(m1,-2).toString(),"-2.0 -4.0 -6.0 \n" +
-                "-8.0 -10.0 -12.0 \n");
-        assertEquals(Matrix.scalarMultiply(m2,0.5).toString(), "1.0 1.0 1.0 \n"+
-                "1.5 1.5 1.5 \n"+
-                "2.0 2.0 2.0 \n");
+        assertEquals("4.0 8.0 \n12.0 16.0 \n", Matrix.scalarMultiply(m, 2).toString());
+        assertEquals("-2.0 -4.0 \n-6.0 -8.0 \n", m.scalarMultiply(-1).toString());
 
-        assertEquals(m1.scalarMultiply(2).toString(),"2.0 4.0 6.0 \n" +
-                "8.0 10.0 12.0 \n"); // Instance Tests
-        assertEquals(m1.scalarMultiply(-2).toString(),"-2.0 -4.0 -6.0 \n" +
-                "-8.0 -10.0 -12.0 \n");
-        assertEquals(m2.scalarMultiply(0.5).toString(), "1.0 1.0 1.0 \n"+
-                "1.5 1.5 1.5 \n"+
-                "2.0 2.0 2.0 \n");
-
-        m1.scalarMultiplyInPlace(2);
-        assertEquals(m1.toString(),"2.0 4.0 6.0 \n" +
-                "8.0 10.0 12.0 \n");
-
-        m1.scalarMultiplyInPlace(-1);
-        assertEquals(m1.toString(),"-2.0 -4.0 -6.0 \n" +
-                "-8.0 -10.0 -12.0 \n");
-
-        m2.scalarMultiplyInPlace(0.5);
-        assertEquals(m2.toString(),"1.0 1.0 1.0 \n"+
-                "1.5 1.5 1.5 \n"+
-                "2.0 2.0 2.0 \n");
-
+        m.scalarMultiplyInPlace(0.5);
+        assertEquals("1.0 2.0 \n3.0 4.0 \n", m.toString());
     }
 
     @Test
-    void multiplyTests(){
+    void testMultiplication() {
+        // Tests matrix multiplication
+        double[][] array1 = {{1, 2}, {3, 4}};
+        double[][] array2 = {{2, 0}, {1, 2}};
+        Matrix m1 = new Matrix(array1);
+        Matrix m2 = new Matrix(array2);
 
-        double[][] test1 = {{1, 2, 3}, {4, 5, 6}};
-        double [][] test2 = {{1,2},{3,4},{5,6}};
-        Matrix m1 = new Matrix(test1);
-        Matrix m2 = new Matrix(test2);
-        Matrix m3 = new Matrix(test1);
-
-        assertEquals(Matrix.multiply(m1,m2).toString(),"22.0 28.0 \n" +
-                "49.0 64.0 \n"); // Static Tests
-
-        assertEquals(Matrix.multiply(m2,m1).toString(),"9.0 12.0 15.0 \n" +
-                "19.0 26.0 33.0 \n"+
-                "29.0 40.0 51.0 \n");
-
-
-        assertEquals(m1.multiply(m2).toString(),"22.0 28.0 \n" +
-                "49.0 64.0 \n"); // Instance Tests
-
-        assertEquals(m2.multiply(m1).toString(),"9.0 12.0 15.0 \n" +
-                "19.0 26.0 33.0 \n"+
-                "29.0 40.0 51.0 \n");
-
+        assertEquals("4.0 4.0 \n10.0 8.0 \n", Matrix.multiply(m1, m2).toString());
+        assertEquals("4.0 4.0 \n10.0 8.0 \n", m1.multiply(m2).toString());
 
         m1.multiplyInPlace(m2);
-        assertEquals(m1.toString(),"22.0 28.0 \n" +
-                "49.0 64.0 \n");
-
-        m2.multiplyInPlace(m3);
-        assertEquals(m2.toString(),"9.0 12.0 15.0 \n" +
-                "19.0 26.0 33.0 \n"+
-                "29.0 40.0 51.0 \n");
+        assertEquals("4.0 4.0 \n10.0 8.0 \n", m1.toString());
     }
 
     @Test
-    void sameSizeTest() {
-        double[][] test1 = {{1, 2, 3}, {4, 5, 6}};
-        double[][] test2 = {{1, 2}, {3, 4}, {5, 6}};
-        Matrix m1 = new Matrix(test1);
-        Matrix m2 = new Matrix(test2);
+    void testIncompatibleAddition() {
+        // Ensures matrices with different dimensions cannot be added
+        double[][] array1 = {{1, 2, 3}, {4, 5, 6}};
+        double[][] array2 = {{1, 2}, {3, 4}, {5, 6}};
+        Matrix m1 = new Matrix(array1);
+        Matrix m2 = new Matrix(array2);
 
         assertThrows(IllegalArgumentException.class, () -> Matrix.add(m1, m2),
                 "Matrices must have the same dimensions.");
     }
+
     @Test
-    void abletoMultiplyTest() {
-        double[][] test1 = {{1, 2, 3}, {4, 5, 6}};
-        Matrix m1 = new Matrix(test1);
-        Matrix m2 = new Matrix(test1);
+    void testIncompatibleMultiplication() {
+        // Ensures incompatible matrices cannot be multiplied
+        double[][] array1 = {{1, 2, 3}, {4, 5, 6}};
+        Matrix m1 = new Matrix(array1);
+        Matrix m2 = new Matrix(array1);
 
         assertThrows(IllegalArgumentException.class, () -> Matrix.multiply(m1, m2),
                 "Columns of Matrix A must be equal to Rows of Matrix B");
     }
-
-
-
-
-
 }
-
-
