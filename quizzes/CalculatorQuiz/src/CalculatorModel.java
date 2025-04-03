0f30cbf55bdf8ef5d762d84e3b8552fb1ca9f857
sebolen <sebolen@uiowa.edu>
2025-04-03 14:57:19 -0500

    Quiz commit

--- quizzes/CalculatorQuiz/src/CalculatorModel.java
+++ quizzes/CalculatorQuiz/src/CalculatorModel.java
@@ -1,4 +1,9 @@
+import jdk.internal.icu.text.UCharacterIterator;
+
+import java.text.NumberFormat;
+
 public class CalculatorModel {
+    private static UCharacterIterator numberFormat;
     private static final NumberFormat formatter = numberFormat.getInstance();
     private double number1;
     private char op=' ';
@@ -17,5 +22,23 @@ public class CalculatorModel {
     public void setOp (char op) {
         this.op = op;
     }
+    public String compute (String display) {
+        String result=" ";
+        try {
+            double number2 = Double.parseDouble(display);
+            switch (op) {
+            case '+': number1 += number2;
+                case '-': number1 -= number2;
+                case '*': number1 *= number2;
+                case '/': number1 /= number2;
+                default: number1 = number2;
 
+            }
+        } catch (Exception e){
+            result = "Err";
+            number1 = 0;
+        }
+        return  result;
+    }
+    
 }
