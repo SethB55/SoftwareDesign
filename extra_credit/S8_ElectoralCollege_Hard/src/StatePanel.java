1e7dc6b3c89bc6cdd444c4396cb981b2124a581d
sebolen <sebolen@uiowa.edu>
2025-05-09 19:38:38 -0500

    Initial Swing test frame works

--- extra_credit/S8_ElectoralCollege_Hard/src/StatePanel.java
+++ extra_credit/S8_ElectoralCollege_Hard/src/StatePanel.java
@@ -1,2 +1,28 @@
-public class StatePanel {  //splitting this up for good programming practice
+
+import javax.swing.*;
+        import java.awt.*;
+
+public class StatePanel extends JPanel {
+    public StatePanel(String stateName, int electoralVotes) {
+        setBorder(BorderFactory.createEtchedBorder());
+        setLayout(new BorderLayout());
+
+        // State name and votes label
+        add(new JLabel(stateName + " (" + electoralVotes + " votes)"), BorderLayout.NORTH);
+
+        // Radio buttons
+        JPanel radioPanel = new JPanel(new GridLayout(3, 1));
+        ButtonGroup group = new ButtonGroup();
+        JRadioButton demButton = new JRadioButton("Democrat");
+        JRadioButton repButton = new JRadioButton("Republican");
+        JRadioButton undecidedButton = new JRadioButton("Undecided", true);
+        group.add(demButton);
+        group.add(repButton);
+        group.add(undecidedButton);
+        radioPanel.add(demButton);
+        radioPanel.add(repButton);
+        radioPanel.add(undecidedButton);
+
+        add(radioPanel, BorderLayout.CENTER);
+    }
 }
